88c1cf131d65a6f2b2d3a15c513979c7923cb682
Sonja Musicki <sonjamusicki@Sonjas-MacBook-Pro.local>
2023-08-27 20:53:36 +0200

    finish socketio exercise

--- websockets/exercise-socketio/backend/server.js
+++ websockets/exercise-socketio/backend/server.js
@@ -19,11 +19,29 @@ const server = http.createServer((request, response) => {
   });
 });
 
-/*
- *
- * Code goes here
- *
- */
+// can create a new socket io server without an http server, but it you pass it one, it will reuse it
+// we need to serve static assets as well, so...
+const io = new Server(server, {});
+
+io.on('connection', (socket) => {
+  console.log(`connected: ${socket.id}`);
+  socket.emit('msg:get', { msg: getMsgs() }); // name of event is arbitrary - client needs to listen to that event by that name
+
+  socket.on('disconnect', () => {
+    console.log(`disconnected: ${socket.id}`);
+  });
+
+  socket.on('msg:post', (data) => {
+    msg.push({
+      user: data.user,
+      text: data.text,
+      time: Date.now()
+    });
+
+    // note: not just socket, but io - io represents entire server
+    io.emit('msg:get', { msg: getMsgs() }); // send msg to everybody
+  })
+})
 
 const port = process.env.PORT || 8080;
 server.listen(port, () =>
--- websockets/exercise-socketio/frontend/socketio-chat.js
+++ websockets/exercise-socketio/frontend/socketio-chat.js
@@ -5,11 +5,25 @@ const msgs = document.getElementById("msgs");
 const presence = document.getElementById("presence-indicator");
 let allChat = [];
 
-/*
- *
- * Code goes here
- *
- */
+// connect and upgrade to ws
+const socket = io('http://localhost:8080');
+
+socket.on('connect', () => {
+  console.log('connected');
+  presence.innerText = '🟢'
+});
+
+socket.on('disconnect', () => {
+  console.log('disconnected');
+  presence.innerText = '🔴'
+});
+
+socket.on('msg:get', (data) => {
+  allChat = data.msg;
+  render();
+})
+// socketio will try to reconnect if the server goes down and then restarts, but you can turn off the option to retry connections
+// also if browser doesn't support ws, will try polling - you can test that by setting window.WebSocket = null
 
 chat.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -18,11 +32,8 @@ chat.addEventListener("submit", function (e) {
 });
 
 async function postNewMsg(user, text) {
-  /*
-   *
-   * Code goes here
-   *
-   */
+  const data = { user, text };
+  socket.emit('msg:post', data)
 }
 
 function render() {
